b9b7315e949e900da68d1826465ae7b2c7549ba5
99Hassan20 <hassanoigag0@gmail.com>
2023-02-16 11:40:10 -0800

    fixed the betty norm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point
+ * Description: 'prints a string the standard output'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
