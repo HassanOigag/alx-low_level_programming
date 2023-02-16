f6469f8d28435ecedc45e136950335de604a2411
99Hassan20 <hassanoigag0@gmail.com>
2023-02-16 11:48:42 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints a string the standard output using printf'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
