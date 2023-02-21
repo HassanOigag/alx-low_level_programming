ea2fb6a2314c32f7a62414176c09c03028bb2281
99Hassan20 <hassanoigag0@gmail.com>
2023-02-21 13:27:33 -0800

    exercise 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints the  alphabet 10 times
+*Return: void (returns nothing)
+*/
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,5 @@
 #include <stdio.h>
 
 void print_alphabet(void);
-
+void print_alphabet_x10(void);
 #endif
