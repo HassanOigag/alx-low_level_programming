118d6aa707d67f708f1f412bca0a675e2e81365e
99Hassan20 <hassanoigag0@gmail.com>
2023-02-21 15:09:56 -0800

    compute the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_abs - computes the absolute value of a number
+*@a: the number int question
+*Return: retuns the absolute value of the number n
+*/
+int	_abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	return (a);
+}
