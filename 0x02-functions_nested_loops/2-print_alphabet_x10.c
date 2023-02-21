407183676814884568891f7be8d295ab83ead55e
99Hassan20 <hassanoigag0@gmail.com>
2023-02-21 13:32:32 -0800

    fixed a bug

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,22 @@
 *print_alphabet_x10 - prints the  alphabet 10 times
 *Return: void (returns nothing)
 */
+
+
 void print_alphabet_x10(void)
 {
 	int i = 1;
+	int c = 'a';
 
 	while (i <= 10)
 	{
-		print_alphabet();
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
 		i++;
 	}
 }
