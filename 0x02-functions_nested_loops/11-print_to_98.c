aab2239fc4601003ac153818d7ac2dd9058a1598
99Hassan20 <hassanoigag0@gmail.com>
2023-02-21 18:00:05 -0800

    added the case if n is greater than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,13 @@
 */
 void print_to_98(int n)
 {
-	while (n < 99)
+	while (n != 98)
 	{
-		if (n != 98)
-			printf("%d, ", n);
-		if (n == 98)
-			printf("%d", n);
-		n++;
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		if (n < 98)
+			n++;
 	}
-	printf("\n");
+	printf("%d\n", n);
 }
