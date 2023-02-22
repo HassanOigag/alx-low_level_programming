4a16109a26875a86715477d5e5abbb25f25e86c5
99Hassan20 <hassanoigag0@gmail.com>
2023-02-21 17:36:50 -0800

    fixed the betty style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *times_table - prints the times table
 *Return: nothing (void)
@@ -8,7 +9,8 @@ void times_table(void)
 	int i = 0;
 	int j = 0;
 	int m = 0;
-	while(i < 10)
+
+	while (i < 10)
 	{
 		j = 0;
 		while (j < 10)
@@ -21,7 +23,7 @@ void times_table(void)
 				if (m <= 9)
 					_putchar(' ');
 			}
-			if (m >= 0 && m < 10) 
+			if (m >= 0 && m < 10)
 				_putchar(m + '0');
 			else
 			{
@@ -32,5 +34,5 @@ void times_table(void)
 		}
 		_putchar('\n');
 		i++;
-	}	
+	}
 }
