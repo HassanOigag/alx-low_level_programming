9d34e8d195f1c6b713104f4505d47559698e7e27
99Hassan20 <hassanoigag0@gmail.com>
2023-02-23 11:27:01 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_numbers - print from 0 to 1
+ *Return: nothing
+ */
+
+void	print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
