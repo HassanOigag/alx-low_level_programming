ab1d987de609b93b2b8068f9769b94547a4c4d9c
99Hassan20 <hassanoigag0@gmail.com>
2023-02-27 14:59:29 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *rest_to_98 - reset a variable to 98
+ *@n: the variable to reset
+ *Return: nothing void
+ */
+
+void resset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void _putchar(int c);
+void reset_to_98(int *n);
+
+#endif
