34fbc7cc61cb30162751829472e19f2049177f21
99Hassan20 <hassanoigag0@gmail.com>
2023-03-07 09:06:38 -0800

    my own memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ *_memset - fills memory with a constant byte
+ *@s: a pointer to the memory block to fill
+ *@b: the constant byte
+ *@n: number of bytes  to fill
+ *Return: returns a pointer to the memory areq
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void _putchar(int c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
