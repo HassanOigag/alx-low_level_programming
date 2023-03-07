71884769e8f5072c91d4968bb9563e05814e3cf4
99Hassan20 <hassanoigag0@gmail.com>
2023-03-07 09:30:54 -0800

    my own memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies memory area
+ *@dest: destination memory area
+ *@src: source memory area
+ *@n: number of bytes to copy
+ *Return: returns a pointer to des
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 void _putchar(int c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
