3c8c4f91a565f93d3927ef55c5019c2bd8016418
99Hassan20 <hassanoigag0@gmail.com>
2023-03-12 08:16:15 -0700

    forgot about betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - entry point
+ *@argc: number of command line arguments
+ *@argv: an array of strings
+ *Return: 0 on success
+ */
+
 int main(int __attribute__ ((unused)) argc, char **argv)
 {
 	printf("%s\n", *argv);
