a5b37a5630c80d386285b2dfd24fcf42aae572a2
99Hassan20 <hassanoigag0@gmail.com>
2023-03-20 06:30:37 -0700

    program name display

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *@argc: number of arguments
+ *@argv: an array of strings
+ *Return: 0 on success
+ */
+
+int main(int __attribute((unused)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
