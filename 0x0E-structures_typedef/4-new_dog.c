8777235d99e719f1c02a919778efae852ab16aa5
99Hassan20 <hassanoigag0@gmail.com>
2023-03-20 12:06:23 -0700

    made copy of strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,32 @@
 #include "dog.h"
 
 /**
+ *_strdup - makes a copy of a string
+ *@s: the string to be copied
+ *Return: the copy of the string s
+ */
+
+char *_strdup(char *s)
+{
+	int i = 0;
+	char *new;
+
+	while (s[i])
+		i++;
+	new = malloc(sizeof(char) * (i + 1));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+/**
  *new_dog - creates a new dog type
  *@name: the dog's name
  *@age: the dog's age
@@ -15,8 +41,12 @@ dog_t	*new_dog(char *name, float age, char *owner)
 	new = (dog_t *) malloc(sizeof(dog_t));
 	if (!new)
 		return (NULL);
-	new->name = name;
+	new->name = _strdup(name);
+	if (!new->name)
+		return (NULL);
 	new->age = age;
-	new->owner = owner;
+	new->owner = _strdup(owner);
+	if (!new->owner)
+		return (NULL);
 	return (new);
 }
