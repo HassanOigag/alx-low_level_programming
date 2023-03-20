22c37776c1060fe6c10f22da8de1804e722cdc91
99Hassan20 <hassanoigag0@gmail.com>
2023-03-20 07:52:17 -0700

    fixed betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - short description
+ *@name: dog's name
+ *@age: dog's
+ *@owner: owner's name
+ *
+ *Longer description
+ */
+
 typedef struct dog
 {
 	char *name;
