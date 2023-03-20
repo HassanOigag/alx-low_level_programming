f722dbaef655935d82b30b0bc8b43a8382396cc1
99Hassan20 <hassanoigag0@gmail.com>
2023-03-20 11:29:16 -0700

    forgot to check the age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 			d->name = "(nil)";
 		if (!d->owner)
 			d->owner = "(nil)";
-		if (d->age < 0)
+		if (!d->age)
 			d->age = 0;
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
