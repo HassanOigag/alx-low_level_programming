d08faf4a765c224c7706551c967dd44f37b67ced
99Hassan20 <hassanoigag0@gmail.com>
2023-04-04 09:58:12 -0700

    more fixse

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp = *head;
 
-	if (!head)
-		return;
 	while (*head)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
-	*head = NULL;
+	head = NULL;
 }
