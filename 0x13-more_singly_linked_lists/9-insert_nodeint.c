010f260a687b60da8ba5fa35d96398181bf7a747
99Hassan20 <hassanoigag0@gmail.com>
2023-04-04 12:47:28 -0700

    more code fixes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 		tmp = tmp->next;
 	}
-	if (!found)
+	if (tmp || !found)
 		return (NULL);
 	node->next = tmp->next;
 	tmp->next = node;
