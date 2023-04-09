74ac722c61f9057719a4256ff1e98c98a03ed32d
99Hassan20 <hassanoigag0@gmail.com>
2023-04-09 14:12:29 -0700

    fixed a bugs and memory leaks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,17 +17,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 	buffer = malloc(sizeof(char) * (letters + 1));
 	if (!buffer)
 		return (0);
 	r = read(fd, buffer, letters);
-	if (r == -1)
+	if (r < -1)
 		return (0);
 	buffer[letters] = '\0';
 	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1 || w != r)
+	if (w < -1 || w != r)
 		return (0);
+	free(buffer);
+	close(fd);
 	return (r);
 }
