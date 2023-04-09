cc3a336ee6c04207d4ee7eff9b6b75722f6bba16
99Hassan20 <hassanoigag0@gmail.com>
2023-04-09 15:22:44 -0700

    fixed a bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	w = 0;
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	if (text_content)
