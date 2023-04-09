1bb21c02072ef0c5c3c42cc416a8dd5f2d7237d8
99Hassan20 <hassanoigag0@gmail.com>
2023-04-09 13:58:46 -0700

    oups

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
+void _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
