9e4752fb77e9e0721409aa1e6d86debe5f343c95
HASSAN OIGAG <57603770+99Hassan20@users.noreply.github.com>
2023-09-08 21:44:08 +0000

    fixed betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,31 +2,31 @@
 
 /**
  * hash_table_print - function that prints a hash table
- * 
+ *
  * @ht: the hash table
  * @return void
  */
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *temp = NULL;
-    int flag = 0;
+	unsigned long int i;
+	hash_node_t *temp = NULL;
+	int flag = 0;
 
-    if (!ht)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp)
-        {
-            if (flag == 1)
-                printf(", ");
-            printf("'%s': '%s'", temp->key, temp->value);
-            flag = 1;
-            temp = temp->next;
-        }
-    }
-    printf("}\n");
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,24 +9,24 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *temp = NULL;
-    hash_node_t *temp2 = NULL;
+	unsigned long int i;
+	hash_node_t *temp = NULL;
+	hash_node_t *temp2 = NULL;
 
-    if (!ht)
-        return;
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp)
-        {
-            temp2 = temp->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-            temp = temp2;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			temp2 = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = temp2;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
