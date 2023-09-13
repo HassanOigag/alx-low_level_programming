9d9f1725f72101fb7c9e5254fa259fbff8331557
Hassan Oigag <hassanoigag0@gmail.com>
2023-09-13 08:33:16 +0100

    more execution fixing

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -35,5 +36,10 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+void hash_table_delete(hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif
